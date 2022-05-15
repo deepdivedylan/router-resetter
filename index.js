2861ded5ba009037dd04452cbebe6d7dc54d86df
Dylan McDonald <dylan@deepdivedylan.io>
2022-05-15 15:56:06 -0700

    navigate the silly router interface to restart

--- index.js
+++ index.js
@@ -5,4 +5,50 @@ const main = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('http://' + process.env.ROUTER_IP);
+    await page.waitForNavigation();
+
+    // event to accept the javascript alert to reboot the router
+    page.on('dialog', async dialog => {
+        await dialog.accept();
+    });
+
+    // sign in
+    await page.click('#user_name');
+    await page.keyboard.type(process.env.ROUTER_USERNAME);
+    await page.click('#loginpp');
+    await page.keyboard.type(process.env.ROUTER_PASSWORD);
+    await page.click('#login_btn');
+    await page.waitForNavigation();
+
+    // click "Management"
+    const management = '#first_menu_manage';
+    await page.waitForSelector(management);
+    await page.click(management);
+
+    // click "Device Management
+    const deviceManagement = 'ul.sec-menu li span[onclick*="menuLeft.changeSecMenu(this,\\"manage\\",1)"]';
+    await page.waitForSelector(deviceManagement);
+    await page.click(deviceManagement);
+
+    // click "Device Reboot"
+    const deviceReboot = 'ul.sec-menu ul.thr-menu.thr-menu-open li[onclick*="menuLeft.changeThrMenu(this,2)"]';
+    await page.waitForSelector(deviceReboot);
+    await page.click(deviceReboot);
+
+    // click "Reboot" in a pesky iframe
+    const iframe = 'iframe[src="reboot.html"]';
+    const reboot = '#Restart_button';
+    await page.waitForSelector(iframe);
+    console.log(page.mainFrame().childFrames());
+    for (const frame of page.mainFrame().childFrames()) {
+        if (frame.url().endsWith('/html/reboot.html')) {
+            await frame.waitForSelector(reboot);
+            await frame.click(reboot);
+            break;
+        }
+    }
+
+    await browser.close();
 };
+
+await main();
